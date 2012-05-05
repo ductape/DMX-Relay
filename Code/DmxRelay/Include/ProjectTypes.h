8d6e0082d3ab5fd6fc68feaae6344eed653ffee0
Shaun Greene <shaun@spgreene.com>
2012-05-04 22:42:00 -0400

    created new project types file to simplify the includes for all the files

---
+++ Code/DmxRelay/Include/ProjectTypes.h
@@ -0,0 +1,19 @@
+/** \file ProjectTypes.h
+    \brief Project Type definitions
+
+    \details Defines custom and common types for use in the application
+ */
+
+
+#ifndef PROJECTTYPES_H_
+#define PROJECTTYPES_H_
+
+/** Define a null as a void pointer to 0 */
+#define NULL ((void *)0)
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <avr/io.h>
+
+
+#endif /* PROJECTTYPES_H_ */
